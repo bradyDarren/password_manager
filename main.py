4f4183d070e9a15951547c0c0a9e9ae67c528adf
bradyDarren <dcgbfinancial@gmail.com>
2025-03-03 12:20:34 -0600

    refactoring of code to appear more readable, changed size of website entry and username entry

--- main.py
+++ main.py
@@ -13,27 +13,25 @@ lock_img = PhotoImage(file= 'logo.png')
 canvas.create_image(100,100,image = lock_img) 
 canvas.grid(column=1, row=0)
 
+#Labels
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
-
 uname = Label(text="Email/Username:")
 uname.grid(column=0, row=2)
-
-uname_entry = Entry(width=35)
-uname_entry.grid(column=1, row=2, columnspan=2)
-
-password = Label(text="Password:", width=21)
+password = Label(text="Password:")
 password.grid(column=0, row=3)
 
+#Entries
+website_entry = Entry(width=38)
+website_entry.grid(column=1, row=1, columnspan=2)
+uname_entry = Entry(width=38)
+uname_entry.grid(column=1, row=2, columnspan=2)
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3)
 
+#Buttons
 generate_button = Button(text="Generate Password")
 generate_button.grid(column=2, row=3)
-
 add_button = Button(text="Add", width=36)
 add_button.grid(column=1, row=4, columnspan=2)
 
